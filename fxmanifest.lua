5529f62bdfe0997145ef067c0a27d6f96b9d998f
div <168165484+divyei@users.noreply.github.com>
2024-08-26 14:47:36 +0800

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+shared_script '@ox_lib/init.lua'
+client_script 'client.lua'
+server_script 'server.lua'
+dependencies {
+    'qb-core',
+    'ox_lib',
+    'ox_target',
+}
