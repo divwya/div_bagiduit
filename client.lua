2b4c74ceb5288f071ccc0a1d4e61143f93353f14
div <168165484+divwya@users.noreply.github.com>
2024-10-28 01:56:32 +0800

    Update client.lua
    kasihan kepada pengguna qb-target 💀

--- client.lua
+++ client.lua
@@ -1,33 +1,71 @@
 local QBCore = exports["qb-core"]:GetCoreObject()
 
-exports.ox_target:addGlobalPlayer({
-    label = 'Give Money',
-    name = 'target_bagiduit',
-    distance = 1.0,
-    onSelect = function(data)
-        local net = NetworkGetPlayerIndexFromPed(data.entity)
-        local id = GetPlayerServerId(net)
-        local input = lib.inputDialog('Give money', {
-            {
-                type = 'number',
-                label = 'Amount',
-                description = 'Max 10,000',
-                min = 1,
-                max = 10000, -- ikut korunk !
-                default = 1,
-            }
-        })
-        if input then
-            local memberi = lib.callback.await('div:bagiduit:target', false, id, input[1])
-            if not memberi then
-                QBCore.Functions.Notify('No enough money')
-            end
-        end
-    end,
-})
+local target = 'ox' --  qbcore
 
-AddEventHandler('onResourceStop', function(resource) -- leh delete
+if target == 'ox' then
+    exports.ox_target:addGlobalPlayer({
+        label = 'Give Money',
+        name = 'target_bagiduit',
+        distance = 1.0,
+        onSelect = function(data)
+            local net = NetworkGetPlayerIndexFromPed(data.entity)
+            local id = GetPlayerServerId(net)
+            local input = lib.inputDialog('Give money', {
+                {
+                    type = 'number',
+                    label = 'Amount',
+                    description = 'Max 10,000',
+                    min = 1,
+                    max = 10000, -- ikut korunk !
+                    default = 1,
+                }
+            })
+            if input then
+                local memberi = lib.callback.await('div:bagiduit:target', false, id, input[1])
+                if not memberi then
+                    QBCore.Functions.Notify('No enough money')
+                end
+            end
+        end,
+    })
+elseif target == 'qbcore' then
+    exports['qb-target']:AddGlobalPed({
+            options = {
+                {
+                    label = 'Give Money',
+                    icon = 'fas fa-user',
+                    action = function(entity)
+                        local net = NetworkGetPlayerIndexFromPed(data.entity)
+                        local id = GetPlayerServerId(net)
+                        local input = lib.inputDialog('Give money', {
+                            {
+                                type = 'number',
+                                label = 'Amount',
+                                description = 'Max 10,000',
+                                min = 1,
+                                max = 10000, 
+                                default = 1,
+                            }
+                        })
+                        if input then
+                            local memberi = lib.callback.await('div:bagiduit:target', false, id, input[1])
+                            if not memberi then
+                                QBCore.Functions.Notify('No enough money')
+                            end
+                        end
+                    end,
+                }
+            },
+        distance = 1.0,
+    })
+end
+
+AddEventHandler('onResourceStop', function(resource)
     if resource == GetCurrentResourceName() then
-        exports.ox_target:removeGlobalPlayer('target_bagiduit')
+        if target == 'ox' then
+            exports.ox_target:removeGlobalPlayer('target_bagiduit')
+        elseif target == 'qbcore' then
+            exports['qb-target']:RemoveGlobalPed('Give money')
+        end
     end
 end)
